404f14050a6709c9b91d0983b6eaa06c529c27e7
Sena Zorlu <senazorlux@gmail.com>
2024-10-19 09:06:22 +0300

    first

---
+++ backend/models/task.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  task: { type: String, required: true },
+  time: { type: String, required: true },
+  date: { type: String, required: true },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
